22406b4c23c60d12689d1ece5bf9d7222a2f0ef1
alberico fagundes <alberico@fagundes.live>
2023-02-25 18:59:12 -0300

    primeiro teste enviar figurinhas

--- index.js
+++ index.js
@@ -13,73 +13,111 @@ client.on("ready", () => {
   );
 });
 
+client.on("message_create", (msg) => {
+  const command = msg.body.split(" ")[0];
+  // Cola seu número onde tem o 84848484, sem o 9
+  const sender = msg.from.includes("96304644") ? msg.to : msg.from;
+  if (command === "/fig") {
+    generateSticker(msg, sender);
+  } 
+  
+  // else {
+  //   msg.reply("❌ codigo desconhecido, tente /fig");
+  // }
+
+});
+
 client.initialize();
-// import { create } from "venom-bot";
-// import pkg from "whatsapp-web.js";
-// const { MessageMedia } = pkg;
 
-// create({
-//   session: "chat-Sticker",
-//   multidevice: true,
-// })
-//   .then((client) => start(client))
-//   .catch((erro) => {
-//     console.log(erro);
-//   });
+const generateSticker = async (msg, sender) => {
+  if (msg.type === "image") {
+    try {
+      const { data } = await msg.downloadMedia();
+      const image = await new MessageMedia("image/jpeg", data, "image.jpg");
+      await client.sendMessage(sender, image, { sendMediaAsSticker: true });
+    } catch (error) {
+      msg.reply("❌ Erro ao processar imagem");
+    }
+  } else {
+    msg.reply("❌ este objeto não parece ser uma imagem");
+  }
+};
 
-// const commands = (client, message) => {
-//   const command = message.body.split(" ")[0];
-//   console.log("message.text", message.text);
-//   console.log("command", command);
+// const generateSticker = async (msg, sender) => {
+//   if(msg.type === "image") {
+//       try {
+//           const { data } = await msg.downloadMedia()
+//           const image = await new MessageMedia("image/jpeg", data, "image.jpg")
+//           await client.sendMessage(sender, image, { sendMediaAsSticker: true })
+//       } catch(e) {
+//           msg.reply("❌ Erro ao processar imagem")}
+//       }
+// // import { create } from "venom-bot";
+// // import pkg from "whatsapp-web.js";
+// // const { MessageMedia } = pkg;
 
-//   const iaCommands = {
-//     sticker: "/fig",
-//     img: "/img",
-//     gpt: "/bot",
-//   };
+// // create({
+// //   session: "chat-Sticker",
+// //   multidevice: true,
+// // })
+// //   .then((client) => start(client))
+// //   .catch((erro) => {
+// //     console.log(erro);
+// //   });
 
-//   if (command === "/fig" || message.text == "/fig") {
-//     const sender = message.from.includes("96304644")
-//       ? message.to
-//       : message.from;
+// // const commands = (client, message) => {
+// //   const command = message.body.split(" ")[0];
+// //   console.log("message.text", message.text);
+// //   console.log("command", command);
 
-//     generateSticker(message, sender);
-//   } else {
-//     message.reply("❌ Erro:codigo nao encontrado ");
-//   }
+// //   const iaCommands = {
+// //     sticker: "/fig",
+// //     img: "/img",
+// //     gpt: "/bot",
+// //   };
 
-//   // switch (command) {
-//   //   case iaCommands.sticker:
-//   //     const sender = message.from.includes("96304644")
-//   //       ? message.to
-//   //       : message.from;
-//   //     generateSticker(message, sender);
+// //   if (command === "/fig" || message.text == "/fig") {
+// //     const sender = message.from.includes("96304644")
+// //       ? message.to
+// //       : message.from;
 
-//   //     break;
-//   // }
+// //     generateSticker(message, sender);
+// //   } else {
+// //     message.reply("❌ Erro:codigo nao encontrado ");
+// //   }
 
-//   // let firstWord = message.text.substring(0, message.text.indexOf(" "));
-//   // console.log("firstWord", firstWord);
-// };
+// //   // switch (command) {
+// //   //   case iaCommands.sticker:
+// //   //     const sender = message.from.includes("96304644")
+// //   //       ? message.to
+// //   //       : message.from;
+// //   //     generateSticker(message, sender);
 
-// async function start(client) {
-//   console.log("startou");
-//   client.onAnyMessage((message) => commands(client, message));
-//   // console.log("client", client);
-//   // console.log("message", message);
-// }
+// //   //     break;
+// //   // }
 
-// const generateSticker = async (message, sender) => {
-//   console.log("message", message);
-//   console.log("sender", sender);
-//   if (message.type === "image") {
-//     try {
-//       const { data } = await message.downloadMedia();
-//       const image = await new MessageMedia("image/jpeg", data, "image.jpg");
-//       await client.sendMessage(sender, image, { sendMediaAsSticker: true });
-//     } catch (error) {
-//       console.log("error", error);
-//       message.reply("❌ Erro ao processar imagem");
-//     }
-//   }
-// };
+// //   // let firstWord = message.text.substring(0, message.text.indexOf(" "));
+// //   // console.log("firstWord", firstWord);
+// // };
+
+// // async function start(client) {
+// //   console.log("startou");
+// //   client.onAnyMessage((message) => commands(client, message));
+// //   // console.log("client", client);
+// //   // console.log("message", message);
+// // }
+
+// // const generateSticker = async (message, sender) => {
+// //   console.log("message", message);
+// //   console.log("sender", sender);
+// //   if (message.type === "image") {
+// //     try {
+// //       const { data } = await message.downloadMedia();
+// //       const image = await new MessageMedia("image/jpeg", data, "image.jpg");
+// //       await client.sendMessage(sender, image, { sendMediaAsSticker: true });
+// //     } catch (error) {
+// //       console.log("error", error);
+// //       message.reply("❌ Erro ao processar imagem");
+// //     }
+// //   }
+// // }
